c064737b1494d9168026ca8824ccaa4651baa7f9
duan <naonao800@163.com>
2021-03-27 14:16:29 +0800

    添加track_total_hits字段，否则新版本es获取数据的total可能不准。

--- duan_elasticsearch.go
+++ duan_elasticsearch.go
@@ -6,8 +6,11 @@ type DuanElasticsearch struct {
 	From string `json:"from,omitempty"`
 	Size string `json:"size,omitempty"`
 
+	// 准确count使用
+	TrackTotalHits bool `json:"track_total_hits,omitempty"`
+
 	// 未实现的预留接口
-	Aggs interface{} `json:"aggs,omitempty"`
-	Source interface{} `json:"source,omitempty"`
-	Sort interface{} `json:"sort,omitempty"`
+	Aggs   interface{} `json:"aggs,omitempty"`
+	Source interface{} `json:"_source,omitempty"`
+	Sort   interface{} `json:"sort,omitempty"`
 }
